66c87f71e8371ced2ad5e46355ca2f38447275d6
catx <ivanshadow19@gmail.com>
2021-09-19 17:38:41 +0700

    Sudah sesuai

---
+++ Assets/Scripts/Nuke.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuke : MonoBehaviour
+{
+    [SerializeField] private int damagePower;
+    [SerializeField] private float nukeRadius;
+}
